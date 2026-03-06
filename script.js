8dff2612f0a3f6dad3305f5a00db52df6bd05288
Shiva Shankar <shivashankar.7991@gmail.com>
2026-03-07 00:47:06 +0530

    updated navbar

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ let scroll = window.pageYOffset;
 hero.style.backgroundPositionY = scroll * 0.5 + "px";
 
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+
+navLinks.classList.toggle("active");
+
+});
